9db9d4431cb13f4d887454231ae8340eca2cc469
Bernard Thomas <79205296+ThomasBernard28@users.noreply.github.com>
2024-11-26 14:50:18 +0100

    Update ProfCoursTest.php question 10

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -282,6 +282,13 @@ class ProfCoursTest extends TestCase
          * s’inspirer de test de sélection et affichage du premier prof pour tester la sélection et l’affichage du premier cours dans la base.
          *
          */
+        $cours = Cours::printOne($conn);
+        $cours_str = $cours->__toString();
+        print "########## - 1e COURS EN BASE - ########## \n";
+        print $cours_str."\n";
+        print "################################################################\n\n";
+        $expected = self::$cours_a[0]->__toString();
+        $this->assertEquals($expected, $cours_str, "Cours \n");
 
 
 
